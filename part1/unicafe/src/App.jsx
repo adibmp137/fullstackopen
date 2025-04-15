c0e974b7d37365c0d5771f55b8e4d947708181d0
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-04-15 03:49:31 +0200

    1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
   return (
     <div>
       <h1>statistics</h1>
