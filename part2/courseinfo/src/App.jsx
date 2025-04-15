4d0e72116ecd2d068039ad198cdeed2034ad1d82
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-04-16 01:20:10 +0200

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,46 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map((p) =>
-      <Part key={p.id} part={p} />
-    )}
-  </div>
-)
-
-const Total = ({parts}) => (
-    <p>
-      <b>
-        total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-      </b>
-    </p>
-)
-
-const Subcourse = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      {course.map((course) => (
-        <Subcourse key={course.id} course={course} />
-      ))}
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
