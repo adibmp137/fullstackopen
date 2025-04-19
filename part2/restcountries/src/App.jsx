8a7fff2e0fbd9e083f4af8448fe732c48c4f66a1
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-04-19 18:23:46 +0200

    2.20.1

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const CountryData = ({ selectedInfo, weather }) => {
+  const imgStyle = {
+    border: '5px solid #555'
+  }
   if (!weather || !weather.current) return null;
   return (
     <div>
@@ -14,10 +17,10 @@ const CountryData = ({ selectedInfo, weather }) => {
           <li key={index}>{language}</li>
         ))}
       </ul>
-      <img src={selectedInfo.flags.png}/>
+      <img src={selectedInfo.flags.png} style={imgStyle}/>
       <h1>Weather in {selectedInfo.capital[0]}</h1>
       <p>Temperature {(weather.current.temp - 273.15).toFixed(2)} Celsius</p>
-      <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`}/>
+      <img src={`https://openweathermap.org/img/wn/${weather.current.weather[0].icon}@2x.png`} alt={`${weather.current.weather[0].description}`}/>
       <p>Wind {weather.current.wind_speed} m/s</p>
     </div>
   )
@@ -27,6 +30,7 @@ const App = () => {
   const [searchType, setSearchType] = useState('')
   const [fullInfo, setInfo] = useState(null)
   const [weather, setWeather] = useState(null)
+  const [forceSelect, setForceSelect] = useState(null)
   const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
   const apiKey = import.meta.env.VITE_SOME_KEY
 
@@ -38,7 +42,7 @@ const App = () => {
       ))
   : []
 
-  const selectedInfo = filteredCountries.length === 1 ? filteredCountries[0] : null
+  const selectedInfo = forceSelect || (filteredCountries.length === 1 ? filteredCountries[0] : null)
   
   useEffect(() => {
     axios.get(baseUrl).then(response => setInfo(response.data))
@@ -63,21 +67,19 @@ const App = () => {
   }
 
   const handleSearchChange = (event) => {
-    const value = event.target.value
-    setSearchType(value)
+    setSearchType(event.target.value)
+    setForceSelect(null)
   };
 
-  const onClick = () => {
-    setSearchType(country.name.common)
-  }
-
   const searchDisplay = 
-  filteredCountries.length < 10
-    ? filteredCountries.length === 1
+  forceSelect
+  ? <CountryData selectedInfo={forceSelect} weather={weather}/>
+  : filteredCountries.length < 10
+    ? filteredCountries.length === 1 
       ? <CountryData selectedInfo={selectedInfo} weather={weather}/>
       : filteredCountries.map((country,i) => (
           <div key={i}>
-            {country.name.common} <button onClick={onClick}>Show</button>
+            {country.name.common} <button onClick={() => setForceSelect(country)}>Show</button>
           </div>))
     : 'Too many matches, specify another filter'
 
