0ddf287bb43ebbc5f9ef23030ef6cf3250a1e270
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-07-07 20:27:22 +0200

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) return null
+  return blogs.reduce((fav, blog) => (fav.likes >= blog.likes ? fav : blog))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
