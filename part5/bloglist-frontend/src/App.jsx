390a2f0b4d6873244c0ba1037080cd3ca56c4659
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-07-13 05:33:40 +0200

    5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -147,9 +147,11 @@ const App = () => {
       <Togglable buttonLabel="new blog" ref={blogFormRef} >
         <BlogForm createBlog = { addBlog } />
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} updateLikes={updateLikes} />
+        )}
     </div>
   )
 }
