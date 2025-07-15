8a855365dc65dac0b61c4c0e8093c7d3461e0a5f
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-07-15 23:18:40 +0200

    5.13

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
-test('renders content', () => {
+test('renders content display title & author, not URL, likes, and nd user', () => {
   const blog = {
     title: 'Test Blog',
     author: 'Test Author',
@@ -20,12 +21,36 @@ test('renders content', () => {
   const authorElement = screen.getByText('Test Author', { exact: false })
   expect(authorElement).toBeDefined()
 
-  const urlElement = screen.queryByText('http://testurl.com')
+  const urlElement = screen.queryByText('http://testurl.com', { exact: false })
   expect(urlElement).toBeNull()
 
-  const likesElement = screen.queryByText('likes 10')
+  const likesElement = screen.queryByText(/likes.*10/)
   expect(likesElement).toBeNull()
 
   const userElement = screen.queryByText('Test User')
   expect(userElement).toBeNull()
+})
+
+test('URL and likes shown when view button clicked', async () => {
+  const blog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 10,
+    user: {
+      name: 'Test User'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const urlElement = screen.getByText('http://testurl.com', { exact: false })
+  expect(urlElement).toBeDefined()
+
+  const likesText = screen.getByText(/likes.*10/)
+  expect(likesText).toBeDefined()
 })
