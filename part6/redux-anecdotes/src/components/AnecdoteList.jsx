3ba9f07f87a5fb21e74f57d35a0bd23d8a1912b3
adibmp137 <153003322+adibmp137@users.noreply.github.com>
2025-07-19 00:35:59 +0200

    6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector( ({ filter , anecdotes }) => {
+    const anecdotesToShow = anecdotes.filter( (anecdote) => 
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()) 
+    )
+    return filter === '' ? anecdotes : anecdotesToShow
+  })
+
   const dispatch = useDispatch()
 
   return (
